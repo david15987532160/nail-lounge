c73b4cd58fb411725fa9099a532fc734442330af
pnqanh <quocanh.inbox@gmail.com>
2021-10-09 00:50:11 +0700

    landing-page: feat: add i18n

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
+import { useRouter } from 'next/router';
+import { MenuFoldOutlined } from '@ant-design/icons';
 import staticData from 'static/assets/data.json';
+import { Dropdown, Menu } from 'antd';
 
 export const NavBar = (props: any) => {
-    const navItems: string[] = staticData.NAV_BAR_ITEMS;
+    const { locale, locales, defaultLocale, asPath } = useRouter();
+    const navItems: string[] = staticData.NAV_BAR_ITEMS[locale as ('en' | 'vi')];
 
     const item = navItems.map((item, i) => {
         const href = `#${ item.toLowerCase() }`;
@@ -19,9 +24,68 @@ export const NavBar = (props: any) => {
         </li>;
     });
 
-    return <div className="sm:absolute sm:top-6 sm:right-44">
-        <ul className="flex justify-center py-4 gap-6 sm:gap-20 text-deep-blue sm:text-white sm:bg-transparent">
-            { item }
-        </ul>
-    </div>
+    const menu = (
+        <Menu>
+            { navItems.map((item, index) =>
+                <Menu.Item key={ index + 1 } className="pr-16">
+                    <a href={ `#${ item.toLowerCase() }` } className="font-poppins text-lg">
+                        { item }
+                    </a>
+                </Menu.Item>)
+            }
+        </Menu>
+    );
+
+    return <>
+        {/*Mobile*/ }
+        <div className="relative sm:hidden">
+            <Dropdown overlay={ menu } placement="bottomLeft" arrow>
+                <MenuFoldOutlined
+                    className="fixed right-3 text-2xl text-white"
+                />
+            </Dropdown>
+        </div>
+
+        {/*Web*/ }
+        <div className="hidden sm:block sm:fixed sm:top-6 sm:right-36">
+            <ul className="flex justify-center py-4 gap-6 sm:gap-16 text-deep-blue sm:text-white sm:bg-transparent">
+                { item }
+            </ul>
+
+            {/*<div className="flex items-center gap-4 text-white">*/}
+            {/*    <div*/}
+            {/*        style={ {*/}
+            {/*            padding: '4px',*/}
+            {/*            marginRight: '4px',*/}
+            {/*        } }*/}
+            {/*    >*/}
+            {/*        <span>Current Language: </span>*/}
+            {/*        <span*/}
+            {/*            style={ {*/}
+            {/*                borderRadius: '3px',*/}
+            {/*                backgroundColor: 'blue',*/}
+            {/*                color: 'white',*/}
+            {/*                padding: '2px',*/}
+            {/*            } }*/}
+            {/*        >*/}
+            {/*  { locale }*/}
+            {/*</span>*/}
+            {/*    </div>*/}
+
+            {/*    <Link*/}
+            {/*        href={ asPath }*/}
+            {/*        locale="en"*/}
+            {/*    >*/}
+            {/*        EN*/}
+            {/*    </Link>*/}
+
+            {/*    <Link*/}
+            {/*        href={ asPath }*/}
+            {/*        locale="vi"*/}
+            {/*    >*/}
+            {/*        VI*/}
+            {/*    </Link>*/}
+            {/*</div>*/}
+        </div>
+    </>
 };
