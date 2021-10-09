c13f2619e66b6cc2101a3cbd009e11f90f29a959
pnqanh <quocanh.inbox@gmail.com>
2021-10-09 21:37:21 +0700

    banner: feat: add popup banner on web loaded

---
+++ components/banner/Banner.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { CloseOutlined } from '@ant-design/icons';
+import styles from 'styles/components/Banner.module.css';
+
+export const Banner = (props: any) => {
+    const [show, setShow] = useState(false);
+
+    const close = () => setShow(false);
+
+    useEffect(() => {
+        setTimeout(() => setShow(true), 500);
+    }, []);
+
+    return <div
+        className={ `${ styles['banner-centered'] } ${ show ? 'fixed' : 'hidden' } w-full sm:w-auto px-3 sm:px-0` }
+    >
+        <CloseOutlined
+            className="absolute sm:top-2 right-4 text-base sm:text-xl text-white"
+            onClick={ close }
+        />
+
+        <img src="/images/banner.jpg" alt="App banner" className=""/>
+    </div>
+}
--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,4 @@ export * from './feature-service';
 export * from './other';
 export * from './combo';
 export * from './footer';
+export * from './banner';
