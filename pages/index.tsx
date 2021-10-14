c8a8edbeb3ced7baa3a3c2a17b70e5c6d6aacd98
pnqanh <quocanh.inbox@gmail.com>
2021-10-14 19:48:43 +0700

    main: feat: add metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,11 +8,27 @@ const Home: NextPage = () => {
     const title = 'Nail Lounge and Spa';
     const { locale = 'en', locales, defaultLocale, asPath } = useRouter();
 
+    const openGraph = {
+        url: 'https://nailloungeandspa.net',
+        image: '/images/nail-lounge-logo.svg',
+        site_name: 'nailLoungeAndSpa',
+        title: 'Nail Lounge and Spa',
+        description: 'Nail Lounge and Spa description',
+        type: 'website',
+        locale: 'en',
+    };
+
     return (
         <div className="overflow-x-hidden">
             <Head>
                 <title>{ title }</title>
                 <link rel="icon" href="/images/nail-lounge-logo.svg"/>
+
+                <meta property="og:url" content={ openGraph.url } key="ogurl"/>
+                <meta property="og:image" content={ openGraph.image } key="ogimage"/>
+                <meta property="og:site_name" content={ openGraph.site_name } key="ogsitename"/>
+                <meta property="og:title" content={ openGraph.title } key="ogtitle"/>
+                <meta property="og:description" content={ openGraph.description } key="ogdesc"/>
             </Head>
 
             <Landing lang={ locale as 'en' | 'vi' }/>
